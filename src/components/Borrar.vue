f5f6b42c8620f6672345b86202b7bc29470d41dc
IvanDMC <75101945+IvanDMC@users.noreply.github.com>
2020-12-15 03:35:54 -0500

    Update Borrar.vue

--- src/components/Borrar.vue
+++ src/components/Borrar.vue
@@ -23,7 +23,7 @@ export default {
         created:function(){
             let self = this
             let nombre_hotel = null
-            //let aux = "https://hotel-pronostico.herokuapp.com/hotel/delete/"-->URL GITHUB
+            //let aux = "https://pronostico-back.herokuapp.com/hotel/delete/"-->URL GITHUB
             let aux = "http://127.0.0.1:8000/hotel/delete/"
             nombre_hotel = document.getElementsByName("nombreHotel")[0].value
             let url = aux+nombre_hotel
